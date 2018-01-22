36f9c8fb4587e24166cb1b4bf6362363263bb978
Ril Matvey <matvej.ril@gmail.com>
2018-01-22 18:45:05 +0700

    first page tempalte

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,33 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-// var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
+var rename = require("gulp-rename");
 var browserSync = require('browser-sync').create();
 
 var configs = {
     src: {
-        less: 'less/index.less'
+        less: 'less/index.less',
+        js: 'scripts/main.js'
     },
     dist: {
-        css: 'css'
+        css: 'css',
+        js: 'scripts'
     },
     watch: {
-        html: 'index.html',
+        html: ['index.html', 'pages/**/*.html'],
         less: 'less/*.less',
-        css: 'css/index.css'
+        css: 'css/index.css',
+        js: 'scripts/**/*.js'
     }
 };
 
-
-gulp.task('default', ['less'], function() {
+gulp.task('default', ['less', 'js'], function() {
     browserSync.init({
         server: "./"
     });
     gulp.watch(configs.watch.html).on('change', browserSync.reload);
     gulp.watch(configs.watch.less, ['less']);
+    gulp.watch(configs.watch.js, ['js']);
 });
 
 gulp.task('less', function() {
@@ -33,3 +37,13 @@ gulp.task('less', function() {
         .pipe(browserSync.stream({ match: '**/*.css' }));
 });
 
+gulp.task('js', function() {
+    return gulp.src(configs.src.js)
+        .pipe(browserify({
+            insertGlobals : true,
+            debug : !gulp.env.production
+        }))
+        .pipe(rename('bundle.js'))
+        .pipe(gulp.dest(configs.dist.js))
+        .pipe(browserSync.stream({match: '**/*.js' }));
+});
