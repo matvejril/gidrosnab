31c3dde43ae27272cf14ad95b82f52d5e0ef3a49
MatvejRil <matvej.ril@gmail.com>
2018-02-02 01:14:28 +0700

    debug nav photos videos catalogs-pdf pages

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,33 +1,40 @@
 (function() {
 
+    var accCatalogs = document.querySelector('.catalog-nav__head');
+    var accCatalogsPDF = document.querySelector('.catalogs-pdf-nav__head');
+
     // Аккордеон навигации каталогов
-    $(document).ready(function() {
-        //прикрепляем клик по заголовкам acc-head
-        $('.catalog-nav__head').on('click', f_acc);
-    });
-    function f_acc(){
-//скрываем все кроме того, что должны открыть
-        $('.catalog-sub-nav').not($(this).next()).slideUp(200);
-// открываем или скрываем блок под заголовком, по которому кликнули
-        $(this).next().slideToggle(500);
+    if (accCatalogs) {
+        $(document).ready(function() {
+            //прикрепляем клик по заголовкам
+            $('.catalog-nav__head').on('click', f_accCatalogs);
+        });
+        function f_accCatalogs(){
+            //скрываем все кроме того, что должны открыть
+            $('.catalog-sub-nav').not($(this).next()).slideUp(200);
+            // открываем или скрываем блок под заголовком, по которому кликнули
+            $(this).next().slideToggle(500);
 
-        // $('.catalog-sub-nav').not($(this)).removeClass('catalog-nav__item_active');
-        // $(this).addClass('catalog-nav__item_active');
+            // $('.catalog-sub-nav').not($(this)).removeClass('catalog-nav__item_active');
+            // $(this).addClass('catalog-nav__item_active');
+        }
     }
-
 
     // Аккордеон навигации кааталогов PDF
-    $(document).ready(function() {
-        //прикрепляем клик по заголовкам acc-head
-        $('.catalogs-pdf-nav__head').on('click', f_acc);
-    });
-    function f_acc(){
-//скрываем все кроме того, что должны открыть
-        $('.catalogs-pdf-sub-nav').not($(this).next()).slideUp(200);
-// открываем или скрываем блок под заголовком, по которому кликнули
-        $(this).next().slideToggle(500);
-
-        // $('.catalog-sub-nav').not($(this)).removeClass('catalog-nav__item_active');
-        // $(this).addClass('catalog-nav__item_active');
+    if (accCatalogsPDF) {
+        $(document).ready(function() {
+            //прикрепляем клик по заголовкам
+            $('.catalogs-pdf-nav__head').on('click', f_accCatalogsPDF);
+        });
+        function f_accCatalogsPDF(){
+            //скрываем все кроме того, что должны открыть
+            $('.catalogs-pdf-sub-nav').not($(this).next()).slideUp(200);
+            // открываем или скрываем блок под заголовком, по которому кликнули
+            $(this).next().slideToggle(500);
+        }
     }
+
+    // $('.catalog-sub-nav').not($(this)).removeClass('catalog-nav__item_active');
+    // $(this).addClass('catalog-nav__item_active');
+
 }());
