ebe75344a734c4e94538ffc76b5d8899bbe4bf61
Ril Matvey <matvej.ril@gmail.com>
2018-02-07 20:08:26 +0700

    danya debuging

--- scripts/main.js
+++ scripts/main.js
@@ -95,6 +95,7 @@ window.onload = function () {
     var indexMapId = document.getElementById('index-map');
     var contactMapId = document.getElementById('contact-map');
 
+
     function init(){
         if(indexMapId){
             indexMap = new ymaps.Map ("index-map", {
@@ -103,12 +104,32 @@ window.onload = function () {
                 zoom: 16
             });
             indexMyPlacemark = new ymaps.Placemark([54.947851, 82.909305], {
-                balloonContent: 'Гидроснаб ТД НМЗ'
+                balloonContent: '<strong>г. Новосибирск, ул. Петухова, 69Б</strong>',
+                iconCaption: 'Гидроснаб ТД НМЗ'
             }, {
-                preset: 'islands#redIcon'
+                preset: 'islands#redDotIcon'
             });
             indexMap.geoObjects.add(indexMyPlacemark);
             indexMap.behaviors.disable('scrollZoom');
+
+            // indexMyPlacemark = new ymaps.Placemark([54.947851, 82.909305], {
+            //     // hintContent: 'Собственный значок метки',
+            //     // iconCaption: 'Гидроснаб ТД НМЗ',
+            //     // iconCaption: 'Гидasdasd asd ',
+            //     balloonContent: '<strong>г. Новосибирск, ул. Петухова, 69Б</strong>'
+            // }, {
+            //     iconCaption: 'Гидроснаб ТД НМЗ',
+            //     // Опции.
+            //     // Необходимо указать данный тип макета.
+            //     iconLayout: 'default#imageWithContent',
+            //     // Своё изображение иконки метки.
+            //     iconImageHref: 'images/yandex-map/yandex-marker.svg',
+            //     // Размеры метки.
+            //     iconImageSize: [72, 72],
+            //     // Смещение левого верхнего угла иконки относительно
+            //     // её "ножки" (точки привязки).
+            //     iconImageOffset: [-35, -64]
+            // });
         }
 
         if (contactMapId) {
@@ -118,9 +139,10 @@ window.onload = function () {
                 zoom: 16
             });
             contactMyPlacemark = new ymaps.Placemark([54.947851, 82.909305], {
-                balloonContent: 'Гидроснаб ТД НМЗ'
+                balloonContent: '<strong>г. Новосибирск, ул. Петухова, 69Б</strong>',
+                iconCaption: 'Гидроснаб ТД НМЗ'
             }, {
-                preset: 'islands#redIcon'
+                preset: 'islands#redDotIcon'
             });
             contactMap.geoObjects.add(contactMyPlacemark);
             contactMap.behaviors.disable('scrollZoom');
