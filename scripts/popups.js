c6bd006948887d285bf9832a87ab9e3a0b0466dc
MatvejRil <matvej.ril@gmail.com>
2018-02-14 01:29:51 +0700

    added scripts popup

--- scripts/popups.js
+++ scripts/popups.js
@@ -1,43 +1,48 @@
 (function() {
 
-    // Диалоговое окно в каталогах pdf
-    var popupWrap = document.querySelector('.popup-wrap');
+    var popupMobile = document.querySelector('.popup-mobile');
+    var popupConf = document.querySelector('.popup-conf');
+
     var catalogDownloadBtn = document.querySelector('.catalogs-pdf-card__download');
-    var callRequestIcon = document.querySelector('.call-request__icon');
-    var callRequestBtn = document.querySelector('.call-request__btn');
-    var closeCallBtn = document.querySelector('.mobile-popup__close');
+    var callRequestIcon = document.querySelectorAll('.call-request__icon');
+    var callRequestBtn = document.querySelectorAll('.call-request__btn');
 
-    // var catalogPopup = document.querySelector('.popup-confidentiality');
-    if (popupWrap && catalogDownloadBtn) {
-        catalogDownloadBtn.addEventListener('click', showDialog);
-        popupWrap.addEventListener('click', hideDialog);
+    var closeMobileBtn = document.querySelector('.popup-mobile__close');
 
-        function showDialog() {
-            popupWrap.style.display = 'block';
+    // Диалоговое окно в каталогах pdf
+    if (catalogDownloadBtn) {
+        catalogDownloadBtn.addEventListener('click', showConfDialog);
+        popupConf.addEventListener('click', hideConfDialog);
+
+        function showConfDialog() {
+            popupConf.style.display = 'block';
         }
-        function hideDialog(e) {
-            if (popupWrap.style.display === 'block' && e.target.className === popupWrap.className) {
-                popupWrap.style.display = 'none';
+        function hideConfDialog(e) {
+            if (e.target === popupConf) {
+                popupConf.style.display = 'none';
             }
         }
     }
 
     // Диалоговое окно в header/footer
-    if (popupWrap && callRequestIcon && callRequestBtn) {
+    if (callRequestIcon && callRequestBtn) {
 
-        callBtn.addEventListener('click', showDialog);
-        popupWrap.addEventListener('click', hideDialog);
-        closeCallBtn.addEventListener('click', hideDialog);
-
-        function showDialog() {
-            popupWrap.style.display = 'block';
+        for (var i = 0; i < callRequestIcon.length; i++) {
+            callRequestIcon[i].addEventListener('click', showMobileDialog);
         }
-        function hideDialog(e) {
-            if (popupWrap.style.display === 'block' && e.target.className === popupWrap.className) {
-                popupWrap.style.display = 'none';
+        for (var z = 0; z < callRequestBtn.length; z++) {
+            callRequestBtn[z].addEventListener('click', showMobileDialog);
+        }
+        popupMobile.addEventListener('click', hideMobileDialog);
+        closeMobileBtn.addEventListener('click', hideMobileDialog);
+
+        function showMobileDialog() {
+            popupMobile.style.display = 'block';
+        }
+        function hideMobileDialog(e) {
+            if (e.target === popupMobile || this === closeMobileBtn) {
+                popupMobile.style.display = 'none';
             }
         }
     }
-
-
 }());
