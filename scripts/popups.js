697116317c4681122b9306442b7e0c03848c829b
Ril Matvey <matvej.ril@gmail.com>
2018-02-13 19:03:26 +0700

    added popup

--- scripts/popups.js
+++ scripts/popups.js
@@ -1,21 +1,43 @@
 (function() {
+
     // Диалоговое окно в каталогах pdf
+    var popupWrap = document.querySelector('.popup-wrap');
+    var catalogDownloadBtn = document.querySelector('.catalogs-pdf-card__download');
+    var callRequestIcon = document.querySelector('.call-request__icon');
+    var callRequestBtn = document.querySelector('.call-request__btn');
+    var closeCallBtn = document.querySelector('.mobile-popup__close');
 
-  var catalogDownloadBtn = document.querySelector('.catalogs-pdf-card__download');
-  var popupWrap = document.querySelector('.popup-wrap');
+    // var catalogPopup = document.querySelector('.popup-confidentiality');
+    if (popupWrap && catalogDownloadBtn) {
+        catalogDownloadBtn.addEventListener('click', showDialog);
+        popupWrap.addEventListener('click', hideDialog);
 
-  // var catalogPopup = document.querySelector('.popup-confidentiality');
-  if (popupWrap && catalogDownloadBtn) {
-      catalogDownloadBtn.addEventListener('click', showDialog);
-      popupWrap.addEventListener('click', hideDialog);
+        function showDialog() {
+            popupWrap.style.display = 'block';
+        }
+        function hideDialog(e) {
+            if (popupWrap.style.display === 'block' && e.target.className === popupWrap.className) {
+                popupWrap.style.display = 'none';
+            }
+        }
+    }
 
-      function showDialog() {
-          popupWrap.style.display = 'block';
-      }
-      function hideDialog(e) {
-          if (popupWrap.style.display === 'block' && e.target.className === popupWrap.className) {
-              popupWrap.style.display = 'none';
-          }
-      }
-  }
+    // Диалоговое окно в header/footer
+    if (popupWrap && callRequestIcon && callRequestBtn) {
+
+        callBtn.addEventListener('click', showDialog);
+        popupWrap.addEventListener('click', hideDialog);
+        closeCallBtn.addEventListener('click', hideDialog);
+
+        function showDialog() {
+            popupWrap.style.display = 'block';
+        }
+        function hideDialog(e) {
+            if (popupWrap.style.display === 'block' && e.target.className === popupWrap.className) {
+                popupWrap.style.display = 'none';
+            }
+        }
+    }
+
+
 }());
