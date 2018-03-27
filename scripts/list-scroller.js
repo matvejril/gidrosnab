8ed92ea1e8a29eb86a28711aac72afc3f4de6274
Ril Matvey <matvej.ril@gmail.com>
2018-03-27 18:05:44 +0700

    debugs catalog className news slider

--- scripts/list-scroller.js
+++ scripts/list-scroller.js
@@ -5,41 +5,43 @@
 
     var newsTrackHeightVal;
 
-    $(window).on('resize init', function () {
-        var newsTrackHeight = newsTrack.css('height');
-        newsTrackHeightVal = Number(newsTrackHeight.replace('px', ''));
-    });
+    var newSm = document.querySelector('.news_sm');
+    if (newSm) {
+        $(window).on('resize init', function () {
+            var newsTrackHeight = newsTrack.css('height');
+            newsTrackHeightVal = Number(newsTrackHeight.replace('px', ''));
+        });
 
-    var newsTrack = $('.news__list-track');
-    var scrollUpBtn = document.querySelector('.news__act-up');
-    var scrollDownBtn = document.querySelector('.news__act-down');
+        var newsTrack = $('.news__list-track');
+        var scrollUpBtn = document.querySelector('.news__act-up');
+        var scrollDownBtn = document.querySelector('.news__act-down');
 
-    scrollUpBtn.addEventListener('click', scrollUp);
-    scrollDownBtn.addEventListener('click', scrollDown);
+        scrollUpBtn.addEventListener('click', scrollUp);
+        scrollDownBtn.addEventListener('click', scrollDown);
 
-    function scrollUp () {
-        var posTop = Number(newsTrack.css('top').replace('px', '')) + 80;
-        if (posTop > 0) {
-            newsTrack.css('top', '0px');
-            scrollUpBtn.style.visibility = "hidden"
-        } else {
-            var newTop = String(posTop + 'px');
-            newsTrack.css('top', newTop);
-            scrollDownBtn.style.visibility = "visible"
+        function scrollUp () {
+            var posTop = Number(newsTrack.css('top').replace('px', '')) + 80;
+            if (posTop > 0) {
+                newsTrack.css('top', '0px');
+                scrollUpBtn.style.visibility = "hidden"
+            } else {
+                var newTop = String(posTop + 'px');
+                newsTrack.css('top', newTop);
+                scrollDownBtn.style.visibility = "visible"
+            }
+            // console.log(newTop);
         }
-        // console.log(newTop);
-    }
-
-    function scrollDown () {
-        var posTop = Number(newsTrack.css('top').replace('px', '')) - 80;
-        if (posTop < -newsTrackHeightVal) {
-            newsTrack.css('top', String(-newsTrackHeightVal));
-            scrollDownBtn.style.visibility = "hidden"
-        } else {
-            var newTop = String(posTop + 'px');
-            newsTrack.css('top', newTop);
-            scrollUpBtn.style.visibility = "visible"
+        function scrollDown () {
+            var posTop = Number(newsTrack.css('top').replace('px', '')) - 80;
+            if (posTop < -newsTrackHeightVal) {
+                newsTrack.css('top', String(-newsTrackHeightVal));
+                scrollDownBtn.style.visibility = "hidden"
+            } else {
+                var newTop = String(posTop + 'px');
+                newsTrack.css('top', newTop);
+                scrollUpBtn.style.visibility = "visible"
+            }
+            // console.log(newTop);
         }
-        // console.log(newTop);
     }
 }());
